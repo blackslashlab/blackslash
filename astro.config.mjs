327aa76d24cb4c378e6d4b0f66cfa921aef7f537
blackslash <blackslashlab@protonmail.com>
2026-09-10 23:31:06 +0530

    Remove base path for github.io repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://blackslashlab.github.io',
-  base: '/blackslash',
   integrations: [
     react(),
     tailwind(),
